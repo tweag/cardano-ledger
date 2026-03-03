de3a24885baabb1a4b1a45ea5db3ec0b4997b654
João Santos Reis <joao.reis@tweag.io>
2026-03-03 18:59:06 +0000

    Make SCLS export path configurable via environment variable

--- eras/conway/impl/canonical-state-test-export/Main.hs
+++ eras/conway/impl/canonical-state-test-export/Main.hs
@@ -1,11 +1,24 @@
-{-# LANGUAGE TypeApplications #-}
-
 module Main where
 
+import System.Environment (lookupEnv)
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Conway.Imp as Imp
 
+pathVarName :: String
+pathVarName = "SCLS_EXPORT_PATH"
+
 main :: IO ()
-main = ledgerTestMain $ do
-  describe "Export SCLS" $
-    Imp.specWithScls "/home/joaosreis/projects/cardano-ledger/dumps" -- TODO: make this path configurable via an environment variable or command line argument
+main = do
+  -- HSpec doesn't do well with extra CLI arguments, so we read the export path from an environment variable or use a default value
+  maybePath <- lookupEnv pathVarName
+  ledgerTestMain $ do
+    describe "Export SCLS" $
+      case maybePath of
+        Just path ->
+          Imp.specWithScls path
+        Nothing ->
+          runIO $ do
+            expectationFailure $
+              "No export path provided, skipping SCLS export tests. Set the "
+                <> pathVarName
+                <> " environment variable to run them."
