7e9c4abab19ef08d1aead3dd145b61d18a4e4d52
João Santos Reis <joao.reis@tweag.io>
2026-03-04 14:36:29 +0000

    Refactor main function

--- eras/conway/impl/canonical-state-test-export/Main.hs
+++ eras/conway/impl/canonical-state-test-export/Main.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import System.Environment (lookupEnv)
+import System.Exit (exitFailure)
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Conway.Imp as Imp
 
@@ -8,17 +11,13 @@ pathVarName :: String
 pathVarName = "SCLS_EXPORT_PATH"
 
 main :: IO ()
-main = do
+main =
   -- HSpec doesn't do well with extra CLI arguments, so we read the export path from an environment variable or use a default value
-  maybePath <- lookupEnv pathVarName
-  ledgerTestMain $ do
-    describe "Export SCLS" $
-      case maybePath of
-        Just path ->
-          Imp.specWithScls path
-        Nothing ->
-          runIO $ do
-            expectationFailure $
-              "No export path provided, skipping SCLS export tests. Set the "
-                <> pathVarName
-                <> " environment variable to run them."
+  lookupEnv pathVarName >>= \case
+    Just path -> ledgerTestMain $ describe "Export SCLS" $ Imp.specWithScls path
+    Nothing -> do
+      putStrLn $
+        "No export path provided, skipping SCLS export tests. Set the "
+          <> pathVarName
+          <> " environment variable to run them."
+      exitFailure
