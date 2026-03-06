060ab0d065cc720a5665ff5f8250a7518985350f
João Santos Reis <joao.reis@tweag.io>
2026-03-06 09:50:33 +0000

    Refactor project structure

--- eras/conway/impl/canonical-state-test-export/Main.hs
+++ eras/conway/impl/canonical-state-test-export/Main.hs
@@ -1,121 +1,21 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import Cardano.Ledger.BaseTypes (ProtVer (ProtVer), Version)
-import Cardano.Ledger.CanonicalState.Conway.Dump (dump, dumpLedgerState, dumpNewEpochState)
-import Cardano.Ledger.CanonicalState.Dump (dumpTx)
+import Cardano.Ledger.CanonicalState.Conway.Export ()
+import Cardano.Ledger.CanonicalState.Export (withScls)
 import Cardano.Ledger.Conway (ConwayEra)
-import Cardano.Ledger.Core (EraPParams (ppProtocolVersionL), eraProtVersions)
-import Cardano.Ledger.Shelley.LedgerState (curPParamsEpochStateL, nesEsL)
-import Data.Aeson (
-  FromJSON,
-  ToJSON (toEncoding),
-  decodeFileStrict,
-  defaultOptions,
-  encode,
-  genericToEncoding,
- )
-import qualified Data.ByteString.Lazy as BSL
-import GHC.Generics (Generic)
-import Lens.Micro ((&), (.~), (^.))
-import System.Directory (createDirectoryIfMissing, doesFileExist)
+import Lens.Micro ((&), (.~))
 import System.Environment (lookupEnv)
 import System.Exit (exitFailure)
-import System.FilePath ((</>))
 import Test.Cardano.Ledger.Common
 import Test.Cardano.Ledger.Conway.Imp (conwayEraGenericSpec)
 import Test.Cardano.Ledger.Conway.ImpTest (
-  ImpInit,
-  LedgerSpec,
   iteSclsDumpHookL,
-  modifyImpInitProtVer,
+  withEachEraVersion,
  )
-import Test.Cardano.Ledger.Imp.Common (withImpInit)
-import Test.Hspec.Core.Spec (Item (..), mapSpecItem_)
-import Test.ImpSpec (ImpInit (impInitEnv))
-
-data Metadata = Metadata
-  { era :: String
-  , protocolVersion :: Version
-  , description :: String
-  , stateCount :: Int
-  }
-  deriving (Generic, Show)
-
-instance ToJSON Metadata where
-  toEncoding = genericToEncoding defaultOptions
-
-instance FromJSON Metadata
-
--- withScls ::
---   FilePath -> SpecWith (ImpInit (LedgerSpec ConwayEra)) -> SpecWith (ImpInit (LedgerSpec ConwayEra))
--- withScls dir =
---   modifyImpInitSclsDumpHook
---     ( \nes tx res -> liftIO $ do
---         dump dir "initial" $ dumpNewEpochState nes
---         let ProtVer version _ = nes ^. nesEsL . curPParamsEpochStateL . ppProtocolVersionL
---         dumpTx dir "txn" version tx
---         case res of
---           Left _failures -> do
---             -- TODO: dump the failures
---             pure ()
---           Right (st, _) -> do
---             dump dir "final" $ dumpLedgerState st -- TODO: this should be dumpNewEpochState, but we don't have the final NewEpochState available here.
---     )
-
-withScls ::
-  Version ->
-  FilePath ->
-  SpecWith (ImpInit (LedgerSpec ConwayEra)) ->
-  SpecWith (ImpInit (LedgerSpec ConwayEra))
-withScls protocolVersion baseDir =
-  mapSpecItem_ $
-    \item@Item
-       { itemRequirement
-       , itemExample = originalItemExample
-       } ->
-        item
-          { itemExample = \p f ->
-              originalItemExample p $ \action ->
-                f $ \impInit ->
-                  action
-                    ( impInit
-                        { impInitEnv =
-                            impInitEnv impInit
-                              & iteSclsDumpHookL
-                                .~ hook itemRequirement
-                        }
-                    )
-          }
-  where
-    hook description nes tx res = do
-      let dir = baseDir </> ("Protocol " <> show protocolVersion) </> description
-      let metadataFile = dir </> "metadata.json"
-      ctx@Metadata {stateCount} <-
-        doesFileExist metadataFile >>= \metadataExists ->
-          if metadataExists
-            then
-              decodeFileStrict metadataFile >>= \case
-                Just ctx -> pure ctx
-                Nothing -> do
-                  -- TODO: clean up the directory if the metadata file is corrupted, to avoid leaving around junk files?
-                  error $ "Failed to decode metadata file: " <> metadataFile
-            else pure $ Metadata {era = "Conway", protocolVersion, description, stateCount = 0}
-      createDirectoryIfMissing True dir
-      dump (dir </> ("initial-" <> show stateCount <> ".scls")) $ dumpNewEpochState nes
-      let ProtVer version _ = nes ^. nesEsL . curPParamsEpochStateL . ppProtocolVersionL
-      dumpTx (dir </> ("txn-" <> show stateCount <> ".cbor")) version tx
-      case res of
-        Left _failures -> do
-          -- TODO: dump the failures
-          pure ()
-        Right (st, _) -> dump (dir </> ("final-" <> show stateCount <> ".scls")) $ dumpLedgerState st -- TODO: this should be dumpNewEpochState, but we don't have the final NewEpochState available here.
-      BSL.writeFile metadataFile $ encode $ ctx {stateCount = stateCount + 1}
 
 pathVarName :: String
 pathVarName = "SCLS_EXPORT_PATH"
@@ -127,12 +27,9 @@ main =
     Just path ->
       ledgerTestMain $
         describe "Export SCLS" $ do
-          withImpInit @(LedgerSpec ConwayEra) $ do
-            forM_ (eraProtVersions @ConwayEra) $ \protVer ->
-              describe ("Protocol " <> show protVer) $
-                modifyImpInitProtVer protVer $
-                  withScls protVer path $
-                    conwayEraGenericSpec @ConwayEra
+          withEachEraVersion @ConwayEra $
+            withScls @ConwayEra (\impInitEnv f -> impInitEnv & iteSclsDumpHookL .~ f) path $
+              conwayEraGenericSpec @ConwayEra
     Nothing -> do
       putStrLn $
         "No export path provided, skipping SCLS export tests. Set the "
---
+++ libs/cardano-ledger-canonical-state/export/Cardano/Ledger/CanonicalState/Export.hs
@@ -0,0 +1,154 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Cardano.Ledger.CanonicalState.Export where
+
+import Cardano.Ledger.BaseTypes (Version)
+import Cardano.Ledger.Binary (
+  EncCBOR (encCBOR),
+  toLazyByteString,
+  toPlainEncoding,
+ )
+-- import Test.Cardano.Ledger.Conway.ImpTest (
+--   LedgerSpec,
+--   iteSclsDumpHookL,
+--   modifyImpInitProtVer,
+--  )
+
+import Cardano.SCLS.CDDL (knownNamespaceKeySizes)
+import Cardano.SCLS.Internal.Entry.ChunkEntry (SomeChunkEntry)
+import Cardano.SCLS.Internal.Serializer.Dump.Plan (
+  SerializationPlan,
+ )
+import Cardano.SCLS.Internal.Serializer.External.Impl (serialize)
+import Cardano.Types.SlotNo (SlotNo (SlotNo))
+import Control.Monad (void)
+import Control.Monad.Trans.Resource (ResIO, runResourceT)
+import Data.Aeson (
+  FromJSON,
+  ToJSON (toEncoding),
+  decodeFileStrict,
+  defaultOptions,
+  encode,
+  genericToEncoding,
+ )
+import qualified Data.ByteString.Lazy as BSL
+import Data.MemPack.Extra (RawBytes)
+import GHC.Generics (Generic)
+import System.Directory (createDirectoryIfMissing, doesFileExist)
+import System.FilePath ((</>))
+import Test.Hspec.Core.Spec (Item (..), SpecWith, mapSpecItem_)
+import Test.ImpSpec (ImpInit (impInitEnv), ImpSpec (ImpSpecEnv))
+
+data Metadata = Metadata
+  { era :: String
+  , protocolVersion :: Version
+  , description :: String
+  , stateCount :: Int
+  }
+  deriving (Generic, Show)
+
+instance ToJSON Metadata where
+  toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON Metadata
+
+dump ::
+  FilePath ->
+  SlotNo ->
+  SerializationPlan (SomeChunkEntry RawBytes) ResIO ->
+  IO ()
+dump filepath slotNo = do
+  -- TODO: should we ignore?
+  void
+    . runResourceT
+    . serialize
+      filepath
+      slotNo
+      knownNamespaceKeySizes
+
+class ExportState era where
+  type ExportLedgerState era
+  type ExportNewEpochState era
+  dumpLedgerState :: ExportLedgerState era -> SerializationPlan (SomeChunkEntry RawBytes) ResIO
+  dumpNewEpochState :: ExportNewEpochState era -> SerializationPlan (SomeChunkEntry RawBytes) ResIO
+  getProtocolVersion :: ExportNewEpochState era -> Version
+
+-- withScls ::
+--   FilePath -> SpecWith (ImpInit (LedgerSpec ConwayEra)) -> SpecWith (ImpInit (LedgerSpec ConwayEra))
+-- withScls dir =
+--   modifyImpInitSclsDumpHook
+--     ( \nes tx res -> liftIO $ do
+--         dump dir "initial" $ dumpNewEpochState nes
+--         let ProtVer version _ = nes ^. nesEsL . curPParamsEpochStateL . ppProtocolVersionL
+--         dumpTx dir "txn" version tx
+--         case res of
+--           Left _failures -> do
+--             -- TODO: dump the failures
+--             pure ()
+--           Right (st, _) -> do
+--             dump dir "final" $ dumpLedgerState st -- TODO: this should be dumpNewEpochState, but we don't have the final NewEpochState available here.
+--     )
+
+withScls ::
+  forall era a tx failures event.
+  (ExportState era, EncCBOR tx) =>
+  ( ImpSpecEnv a ->
+    (ExportNewEpochState era -> tx -> Either failures (ExportLedgerState era, event) -> IO ()) ->
+    ImpSpecEnv a
+  ) ->
+  FilePath ->
+  SpecWith (ImpInit a) ->
+  SpecWith (ImpInit a)
+withScls setHook baseDir =
+  mapSpecItem_ $
+    \item@Item
+       { itemRequirement
+       , itemExample = originalItemExample
+       } ->
+        item
+          { itemExample = \p f ->
+              originalItemExample p $ \action ->
+                f $ \impInit ->
+                  action
+                    ( impInit
+                        { impInitEnv = setHook (impInitEnv impInit) (hook itemRequirement)
+                        }
+                    )
+          }
+  where
+    hook description nes tx res = do
+      let protocolVersion = getProtocolVersion @era nes
+      let dir = baseDir </> ("Protocol " <> show protocolVersion) </> description
+      let metadataFile = dir </> "metadata.json"
+      ctx@Metadata {stateCount} <-
+        doesFileExist metadataFile >>= \metadataExists ->
+          if metadataExists
+            then
+              decodeFileStrict metadataFile >>= \case
+                Just ctx -> pure ctx
+                Nothing -> do
+                  -- TODO: clean up the directory if the metadata file is corrupted, to avoid leaving around junk files?
+                  error $ "Failed to decode metadata file: " <> metadataFile
+            else pure $ Metadata {era = "Conway", protocolVersion, description, stateCount = 0}
+      createDirectoryIfMissing True dir
+      let initialSlotNo = SlotNo 1 -- TODO: use the actual slot number if available
+      dump (dir </> ("initial-" <> show stateCount <> ".scls")) initialSlotNo $ dumpNewEpochState @era nes
+      -- Dump tx
+      BSL.writeFile
+        (dir </> ("txn-" <> show stateCount <> ".cbor"))
+        (toLazyByteString (toPlainEncoding protocolVersion (encCBOR tx)))
+      case res of
+        Left _failures -> do
+          -- TODO: dump the failures
+          pure ()
+        Right (st, _) -> do
+          let finalSlotNo = SlotNo 1 -- TODO: use the actual slot number if available
+          dump (dir </> ("final-" <> show stateCount <> ".scls")) finalSlotNo $ dumpLedgerState @era st -- TODO: this should be dumpNewEpochState, but we don't have the final NewEpochState available here.
+      BSL.writeFile metadataFile $ encode $ ctx {stateCount = stateCount + 1}
